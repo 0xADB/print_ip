f3bd52502c08114d5d57109eea6ca2f25a77bde8
0xADB <ad.beregovoy@gmail.com>
2018-11-20 23:55:17 +0300

    added sources (mistakenly forgotten before)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include "print_ip.h"
 
 #include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <tuple>
 
 int main (int, char **)
 {
@@ -8,6 +12,11 @@ int main (int, char **)
   ip::printer<short>()(std::cout, 0); std::cout << '\n';
   ip::printer<int>  ()(std::cout, 2130706433); std::cout << '\n';
   ip::printer<long> ()(std::cout, 8875824491850138409); std::cout << '\n';
+
+  ip::printer<std::string>()(std::cout, std::string("192.168.101.102")); std::cout << '\n';
+  ip::printer<std::list<int>>()(std::cout, std::list<int>({192,168,101,103})); std::cout << '\n';
+  ip::printer<std::vector<short>>()(std::cout, std::vector<short>({192,168,101,104})); std::cout << '\n';
+  ip::printer<std::tuple<long, long, long>>()(std::cout, std::make_tuple(192l,168l,101l)); std::cout << '\n';
 
   return 0;
 }
--- print_ip.h
+++ print_ip.h
@@ -3,6 +3,7 @@
 #include <ostream>
 #include <type_traits>
 #include <stdint.h>
+#include <tuple>
 
 namespace ip
 {
@@ -12,7 +13,10 @@ namespace ip
     {};
 
   template<typename T>
-    struct printer<T, typename std::enable_if<std::is_integral<T>::value>::type>
+    struct printer<
+      T
+      , typename std::enable_if<std::is_integral<T>::value>::type
+      >
     {
       void operator()(std::ostream& stream, const T& addr)
       {
@@ -23,6 +27,74 @@ namespace ip
 	  stream << static_cast<int>(*(last));
 	  if (first != last) stream << '.';
 	}
+      }
+    };
+
+  template<typename ContainerT>
+    struct printer<
+      ContainerT
+      , typename std::enable_if<std::is_integral<typename ContainerT::value_type>::value>::type
+      >
+    {
+      void operator()(std::ostream& stream, const ContainerT& addr)
+      {
+	for (auto it = std::begin(addr)
+	    , end = std::end(addr)
+	    ; it != end
+	    ; ++it
+	    )
+	{
+	  if (it != std::begin(addr)) stream << '.';
+	  stream << static_cast<int>(*it);
+	}
+      }
+    };
+
+  template<>
+    struct printer<std::string>
+    {
+      void operator()(std::ostream& stream, const std::string& addr)
+      {
+	stream << addr.c_str();
+      }
+    };
+
+  template<typename T, typename... Args>
+    struct printer<std::tuple<T, Args...>>
+    {
+      using TupleT = std::tuple<T,Args...>;
+
+      // to localize static_cast
+      void print(std::ostream& stream, const T& byte)
+      {
+	stream << static_cast<int>(byte);
+      }
+
+      void print(std::ostream& stream, char delimiter, const T& byte)
+      {
+	stream << delimiter;
+	print(stream, byte);
+      }
+
+      template<typename U, size_t I = 0>
+	typename std::enable_if<std::is_same<U,T>::value>::type
+	  operator()(std::ostream& stream, const TupleT& addr)
+      {
+	print(stream, std::get<I>(addr));
+      }
+
+      template<typename U, typename... Brgs, size_t I = sizeof...(Brgs)>
+	typename std::enable_if<std::is_same<U,T>::value && sizeof...(Brgs)>::type
+	  operator()(std::ostream& stream, const TupleT& addr)
+      {
+	operator()<Brgs...>(stream, addr);
+	print(stream, '.', std::get<I>(addr));
+      }
+
+      void operator()(std::ostream& stream, const TupleT& addr)
+      {
+	this->operator()<Args...>(stream, addr);
+	print(stream, '.', std::get<sizeof...(Args)>(addr));
       }
     };
 
